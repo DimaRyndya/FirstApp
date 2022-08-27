08290c740941d3d976ab39acbd2a29b462ad682b
DimaRyndya <dmitriy.ryndya@gmail.com>
2022-08-27 17:15:03 +0300

    Initial Commit

---
+++ FirstApp/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  FirstApp
+//
+//  Created by Dmitriy Ryndya on 27.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ByeView()
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Dmitriy Ryndya on 27.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
